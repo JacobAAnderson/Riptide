2ecd3bc6a5714c26a1884b6693c6a0175e0519e1
Jacob Anderson <jacobanderson1985@me.com>
2020-10-26 17:43:26 -0700

    Adding File transfer Bash Scripts

---
+++ TransferFiles.sh
@@ -0,0 +1,35 @@
+#!/usr/local/bin/bash
+
+name="Dory"
+
+ip=192.168.1.210
+
+echo " "
+echo " "
+echo " "
+echo "Starting File transfer from Riptide: $name"
+echo "At: $ip"
+echo " "
+
+echo "---> Getting pAcommsHandler logs"
+scp uuuv@$ip:pAcommsHandler* pAcommsHandler/
+scp uuuv@$ip:Jake/pAcommsHandler* pAcommsHandler/
+
+rm pAcommsHandler/pAcommsHandler_uuuv_latest.txt
+chmod 444 pAcommsHandler/*pAcommsHandler*
+
+echo " "
+echo "--> Getting Acomms Logs"
+scp uuuv@$ip:Jake/logs/*acommsLog.csv Acoms/
+chmod 444 Acoms/*acommsLog.csv
+
+echo " "
+echo "--> Getting Data Logs"
+scp uuuv@$ip:Jake/logs/*DataLog.csv Data/
+chmod 444 Data/*DataLog.csv
+echo " "
+echo " "
+echo "Done"
+echo " "
+echo " "
+echo " "
